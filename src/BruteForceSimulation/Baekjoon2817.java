1a3d9bf3cef074dce19bbbf9680c380838a24b1c
Vivatube <dltkdgus2008@naver.com>
2026-01-20 18:15:13 +0900

    Baekjoon2817 ALPS 투표

--- src/BruteForceSimulation/Baekjoon2817.java
+++ src/BruteForceSimulation/Baekjoon2817.java
@@ -8,47 +8,48 @@ public class Baekjoon2817 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int X = Integer.parseInt(br.readLine()); // 참가자의 수 X
         int N = Integer.parseInt(br.readLine()); // 총 스태프의 수
+        int[] frequency = new int[26]; // 점수 집합에서 알파벳별 등장 빈도를 체크하기 위한 배열
+        int[] chipArray = new int[26]; // 스태프가 받은 칩 개수를 저장하기 위한 배열
+        boolean[] isOver = new boolean[26]; // 5% 이상 득표율인 스태프면 무조건 이름 출력
         if (N == 0) return; // 스태프가 없는 경우는 아무것도 출력할 필요 없음.
-        char[] staffName = new char[N]; // 스태프의 이름을 저장할 배열
-        int[] vote = new int[N]; // 각 스태프가 받은 득표수를 저장할 배열
-        int total = 0; // 총 득표수를 구하기 위한 변수
-        for (int i = 0; i < N; i++) { // N명의 스태프에 대한 데이터를 입력받음.
+        List<Staff> list = new ArrayList<>(); // 점수 집합을 저장하기 위한 List
+        for (int i = 0; i < N; i++) { // 스태프 수만큼 점수를 입력받는다.
             StringTokenizer st = new StringTokenizer(br.readLine());
-            staffName[i] = st.nextToken().charAt(0); // 스태프의 이름을 입력받음
-            vote[i] = Integer.parseInt(st.nextToken()); // 득표수를 입력받음
-            total += vote[i]; // 득표수를 누적함.
+            int index = st.nextToken().charAt(0) - 'A'; // 입력받은 알파벳을 인덱스로서 활용한다.
+            int chip = Integer.parseInt(st.nextToken()); // 해당 스태프가 받은 칩의 개수
+            chipArray[index] = chip; // 해당 스태프가 받은 칩 개수를 저장한다.
         }
-        // 커트라인이 될 득표수의 5%를 구함.
-        double cutLine = total * 0.05; // double과의 연산이므로 double로 받음.
-        ArrayList<ScoreSet> list = new ArrayList<>(); // 점수 집합을 담기 위한 List
-        // 커트라인 이상의 득표수를 얻은 스태프만 점수 집합을 생성한다.
-        for (int i = 0; i < N; i++) {
-            if (vote[i] >= cutLine) { // 커트라인을 넘은 스태프만
-                ScoreSet set = new ScoreSet(i, vote[i] / (double)i); // 이름 인덱스와 점수로 점수집합을 생성하고
-                list.add(set); // 리스트에 담는다.
+        double minimum = X * 0.05;
+        for (int i = 0; i < 26; i++) { // 받은 칩개수를 토대로 5% 이상의 득표율일 때만 점수집합을 만든다.
+            if (chipArray[i] >= minimum) {
+                isOver[i] = true;
+                for (int j = 1; j <= 14; j++) {
+                    double score = chipArray[i] / (double)j; // 실수 연산을 위해 double로 형변환 후 나눗셈을 수행
+                    Staff staff = new Staff(i, score); // 인덱스와 점수를 기반으로 Staff를 생성하고
+                    list.add(staff); // 점수 집합에 누적한다.
+                }
             }
         }
-        // 점수를 기준으로 내림차순 정렬한다.
-        list.sort((s1, s2) -> Double.compare(s2.score, s1.score));
-        // 상위 14개의 점수에 대해 알파벳 순서로 출력을 위해 카운팅한다.
-        int[] frequency = new int[N]; // staffName과 매핑을 위해 frequency 빈도 배열을 생성함.
+        // 점수 집합을 점수 기준으로 내림차순 정렬한다. ( 점수가 높은 사람이 먼저 오게 )
+        list.sort((s1, s2) -> Double.compare(s2.score, s1.score)); // 실수 2개를 내림차순 정렬한다.
         for (int i = 0; i < 14; i++) {
-           frequency[list.get(i).index]++; // 카운팅한다.
+            frequency[list.get(i).index]++; // 상위 14명에 대해서 등장 빈도를 센다.
         }
-        // 카운팅된 배열을 확인해서 빈도수만큼 매핑된 알파벳을 출력한다.
-        for (int i = 0; i < N; i++) {
-            for (int count = 0; count < frequency[i]; count++) { // count만큼
-                System.out.print(staffName[i]);
-            }
+        // 빈도 배열을 세면서 등장하는 알파벳만 그 횟수인 빈도를 같이 출력한다.
+        // 이렇게 하면 알파벳이 오름차순으로 출력됨.
+        for (int i = 0; i < 26; i++) {
+            if (isOver[i]) { // 득표율이 5%를 넘은 스태프에 대해서는
+            System.out.println((char)('A' + i) + " " + frequency[i]); // 알파벳과 등장 빈도를 출력한다.
         }
     }
 }
+}
 
-class ScoreSet { // 점수 집합
-    int index; // 이름이 담긴 인덱스
-    double score; // 점수
+class Staff {
+    int index; // 알파벳의 인덱스
+    double score; // 점수 집합의 점수
 
-    ScoreSet(int index, double score) {
+    Staff(int index, double score) {
         this.index = index;
         this.score = score;
     }
