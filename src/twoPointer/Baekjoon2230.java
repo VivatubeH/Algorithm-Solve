f466cf9a6c43e2ee9a147dcaf271c983af0ce8b4
Vivatube <dltkdgus2008@naver.com>
2026-01-28 15:17:18 +0900

    Baekjoon2230 수고르기

--- src/twoPointer/Baekjoon2230.java
+++ src/twoPointer/Baekjoon2230.java
@@ -1,4 +1,35 @@
 package twoPointer;
+import java.io.*;
+import java.util.*;
 
 public class Baekjoon2230 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer firstLine = new StringTokenizer(br.readLine());
+        int size = Integer.parseInt(firstLine.nextToken());
+        int target = Integer.parseInt(firstLine.nextToken());
+
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            array[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(array);
+
+        int right = 0;
+        int min = Integer.MAX_VALUE;
+
+        for (int left = 0; left < size; left++) {
+            while (right < size && array[right] - array[left] < target) {
+                right++;
+            }
+            if (right != size && array[right] - array[left] >= target) {
+                if (array[right] - array[left] < min) {
+                    min = array[right] - array[left];
+                }
+            }
+        }
+        System.out.print(min);
+    }
 }
