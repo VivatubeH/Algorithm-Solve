763b088d0046679762b31aedc0467b7e05404883
Vivatube <dltkdgus2008@naver.com>
2026-02-04 17:12:44 +0900

    docs: 알고리즘 관련 문서 이관

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,15 @@
+import java.io.*;
+import java.util.*;
+
 public class Test {
     public static void main(String[] args) {
 
+    }
+}
+
+class Book implements Comparable<Book> {
+
+    public int compareTo(Book b) {
+        return 0;
     }
 }
